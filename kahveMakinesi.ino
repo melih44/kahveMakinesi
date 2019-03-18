8af8483adfc4fe8d757ce97163bc4f58ee18479e
melih44 <48682285+melih44@users.noreply.github.com>
2019-03-18 22:18:06 +0300

    Update kahveMakinesi.ino
    Kodlara yorum satırı eklendi.

--- kahveMakinesi.ino
+++ kahveMakinesi.ino
@@ -1,32 +1,32 @@
-#include <Servo.h>
-#include <LiquidCrystal.h>
-#include <SoftwareSerial.h>
-SoftwareSerial bt(10,11);
-LiquidCrystal lcd(8,7,6,5,4,3);
-Servo servoKahve;
-Servo servoVana;
-int isiticiRole=9;
-char komut;
+#include <Servo.h> // Servo Kütüphanesi eklendi.
+#include <LiquidCrystal.h> // LCD Kütüphanesi eklendi.
+#include <SoftwareSerial.h> // Arduinonun TX RX uçları arızalı olduğu için bluetooth modülünde kullanılmak üzere kütüphane eklendi.
+SoftwareSerial bt(10,11);  // bluetooth bağlantıları
+LiquidCrystal lcd(8,7,6,5,4,3); // lcd bağlantıları
+Servo servoKahve; // servo tanıtıldı
+Servo servoVana; // servo tanıtıldı
+int isiticiRole=9; // ısıtıcı röleyi 9. pinden kontrol edeceğiz
+char komut; // bluetooth kontrolü için tanımlandı
 
 void setup()
 {
-  servoKahve.attach(12);
-  servoVana.attach(13);
-  lcd.begin(16,2);
-  bt.begin(9600);
-  pinMode(isiticiRole,OUTPUT);
-  digitalWrite(9,HIGH);
-  servoKahve.write(120);
-  servoVana.write(0); 
+  servoKahve.attach(12); // servoKahve 12.pin
+  servoVana.attach(13); // servoVana 13.pin
+  lcd.begin(16,2); // lcd 
+  bt.begin(9600); // bluetooth 
+  pinMode(isiticiRole,OUTPUT); // röleyi çıkış olarak tanımladık
+  digitalWrite(9,HIGH); // röleyi başlangıç konumunu 1 yaptık (negatif kenar tetikleme.)
+  servoKahve.write(120); // servoKahvenin başlangıç konumu
+  servoVana.write(0); // servoVana başlangıç konumu
 }
 
 void loop()
 {  
-  if (bt.available()>0)
+  if (bt.available()>0) // bluetooth kontrol edildi
   {
     komut = bt.read();
-    if(komut == 'y')
-    { 
+    if(komut == 'y') // MITnin sunmuş olduğu uygulamada yazılan kodda 'y' olarak tanımlandı. (fotoğraflarda mevcut.)
+    { // kodlar
       lcd.setCursor(0,0);
       lcd.print("    Kahveniz    ");
       lcd.setCursor(0,1);
@@ -49,8 +49,8 @@ void loop()
       lcd.setCursor(0,1);
       lcd.print("  Afiyet Olsun  "); 
      }
-    if(komut == 's')
-    {
+    if(komut == 's') // MITnin sunmuş olduğu uygulamada yazılan kodda 's' olarak tanımlandı. (fotoğraflarda mevcut.)
+    { // kodlar
         lcd.setCursor(0,0);
         lcd.print("    Kahveniz    ");
         lcd.setCursor(0,1);
